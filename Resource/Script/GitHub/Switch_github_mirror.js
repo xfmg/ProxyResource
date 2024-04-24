94836752850eb65d99c9d288e137b42f2f527391
palapal <palapal708@cumzle.com>
2024-04-24 16:11:09 +0800

    更新

--- Resource/Script/GitHub/Switch_github_mirror.js
+++ Resource/Script/GitHub/Switch_github_mirror.js
@@ -3,8 +3,8 @@ Loon专用
 2024-04-21 01:29:51
 */
 let githubPrefix = "https://raw.githubusercontent.com/"
-let fastrawPrefix = "https://fastraw.ixnic.net/" //由FastGit群组成员 @duya1234567 提供
-let hubinceptPrefix = "https://hub.incept.pw/" // 由FastGit群组成员 @mxe365 提供
+let fastrawPrefix = "https://fastraw.ixnic.net/" //由FastGit群组成员 @duya1234567 提供，代号A镜像。
+let hubinceptPrefix = "https://hub.incept.pw/" // 由FastGit群组成员 @mxe365 提供，代号B镜像。
 // let kkgithubPrefix = "https://raw.kkgithub.com/" //由KGithub提供，暂时失效。
 
 //1: fastraw.ixnic.net 2.hub.incept.pw 3.raw.kkgithub.com
@@ -20,13 +20,13 @@ if (!url.startsWith(githubPrefix)) {
     return;
 }
 
-if (changeTo == "fastraw.ixnic.net") {
-    headers["host"] = "fastraw.ixnic.net"
+if (changeTo == "") {
+    headers["host"] = "A镜像"
     url = url.replace(githubPrefix,fastrawPrefix)
-} else if (changeTo == "hub.incept.pw") {
+} else if (changeTo == "B镜像") {
     headers["host"] = "hub.incept.pw"
     url = url.replace(githubPrefix,hubinceptPrefix)
-} else if (changeTo == "raw.kkgithub.com") {
+} else if (changeTo == "C镜像") {
     headers["host"] = "raw.kkgithub.com"
     url = url.replace(githubPrefix,kkgithubPrefix)
 }
