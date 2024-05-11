66604917978aafa7557f1720450ba4097547d5e2
palapal <palapal708@cumzle.com>
2024-05-11 23:27:09 +0800

    更新

--- Resource/Script/GaoDing/GaoDing_remove_ads.js
+++ Resource/Script/GaoDing/GaoDing_remove_ads.js
@@ -1,6 +1,10 @@
-// 2024-05-11 23:04:07
+// 2024-05-11 23:26:52
 var json = JSON.parse($response.body);
-var banner = json.pits[0];
-banner.delivery_materials = "";
+var banner = json.pits[0].delivery_materials;
+banner.forEach(function (bannerData) {
+    for (var prop in bannerData) {
+        bannerData[prop] = "";
+    }
+});
 console.log(json);
 $done({ body: JSON.stringify(json) });
