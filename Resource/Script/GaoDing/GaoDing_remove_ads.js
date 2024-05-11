cca9836f95166cc86d86a1d5ea8c083fecfb6b23
palapal <palapal708@cumzle.com>
2024-05-11 22:57:14 +0800

    更新

--- Resource/Script/GaoDing/GaoDing_remove_ads.js
+++ Resource/Script/GaoDing/GaoDing_remove_ads.js
@@ -1,4 +1,5 @@
-// 2024-05-11 18:12:16
-var banner = JSON.parse($response.body);
-delete banner.pits[0].delivery_materials;
-$done();
+// 2024-05-11 22:56:07
+var json = JSON.parse($response.body);
+var banner = json.pits[0];
+banner.delivery_materials = "";
+$done({ body: JSON.stringify(json) });
