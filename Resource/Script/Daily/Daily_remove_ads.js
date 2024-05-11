b58b25dc738206901751c12ff8b85f066740a14a
palapal <palapal708@cumzle.com>
2024-05-11 18:18:18 +0800

    更新

--- Resource/Script/Daily/Daily_remove_ads.js
+++ Resource/Script/Daily/Daily_remove_ads.js
@@ -4,9 +4,9 @@
 let obj = JSON.parse($response.body);
 obj.data.splashConfNew.forEach(item => {
   item.aliveTime = 0;
- item.urlX = "";
- item.url = "";
- item.title = "去开屏广告";
- item.status = item.status.replace("ON", "OFF");
+  item.urlX = "";
+  item.url = "";
+  item.title = "去开屏广告";
+  item.status = item.status.replace("ON", "OFF");
 });
-$done({body: JSON.stringify(obj)});
+$done({ body: JSON.stringify(obj) });
