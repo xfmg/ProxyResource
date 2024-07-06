850d880fab95b80717aa73a0276d4a6842a9cd24
palapal <palapal708@cumzle.com>
2024-07-06 23:15:15 +0800

    更新

--- Resource/Script/GaoDing/GaoDing_remove_ads.js
+++ Resource/Script/GaoDing/GaoDing_remove_ads.js
@@ -2,21 +2,19 @@
 
 var json = JSON.parse($response.body);
 
-// 检查 URL 路径是否包含 /oc/exhibitions/template/resources
 if ($request.url.includes("/oc/exhibitions/template/resources")) {
-    // 移除 pits[0].delivery_materials
+    // 移除首页轮播图
     if (json.pits && json.pits[0]) {
         delete json.pits[0].delivery_materials;
     }
-    // 移除 pits[1].materials
+    // 移除首页会员入口
     if (json.pits && json.pits[1]) {
         delete json.pits[1].materials;
     }
 }
 
-// 检查 URL 路径是否包含 /oc/exhibitions/app_mine/resources
 if ($request.url.includes("/oc/exhibitions/app_mine/resources")) {
-    // 移除 pits[2].delivery_materials
+    // 移除我的页面轮播图
     if (json.pits && json.pits[2]) {
         delete json.pits[2].delivery_materials;
     }
