99d5a9c16daafb4bd6d09b50cb7a808b835a79c7
palapal <palapal708@cumzle.com>
2024-07-06 22:43:13 +0800

    更新

--- Resource/Script/GaoDing/GaoDing_remove_ads.js
+++ Resource/Script/GaoDing/GaoDing_remove_ads.js
@@ -1,27 +1,24 @@
-// 2024-07-06 22:29:43
+// 2024-07-06 22:43:04
+
 var json = JSON.parse($response.body);
 
 // 检查 URL 路径是否包含 /oc/exhibitions/template/resources
 if ($request.url.includes("/oc/exhibitions/template/resources")) {
-    // 设置 pits[2].delivery_materials 的所有属性值为空字符串
+    // 移除 pits[0].delivery_materials
     if (json.pits && json.pits[0] && json.pits[0].delivery_materials) {
-        json.pits[0].delivery_materials.forEach(function (bannerData) {
-            for (var prop in bannerData) {
-                bannerData[prop] = "";
-            }
-        });
+        json.pits[0].delivery_materials = [];
+    }
+    // 移除 pits[1]
+    if (json.pits && json.pits[1]) {
+        json.pits[1] = {};
     }
 }
 
 // 检查 URL 路径是否包含 /oc/exhibitions/app_mine/resources
 if ($request.url.includes("/oc/exhibitions/app_mine/resources")) {
-    // 设置 pits[2].delivery_materials 的所有属性值为空字符串
-    if (json.pits && json.pits[2] && json.pits[2].delivery_materials) {
-        json.pits[2].delivery_materials.forEach(function (bannerData) {
-            for (var prop in bannerData) {
-                bannerData[prop] = "";
-            }
-        });
+    // 移除 pits[2]
+    if (json.pits && json.pits[2]) {
+        json.pits[2] = {};
     }
 }
 
