1836d38f7e2b6acdfcc93773e40f21bf25b71a20
palapal <palapal708@cumzle.com>
2024-07-06 23:04:33 +0800

    更新

--- Resource/Script/GaoDing/GaoDing_remove_ads.js
+++ Resource/Script/GaoDing/GaoDing_remove_ads.js
@@ -8,17 +8,14 @@ if ($request.url.includes("/oc/exhibitions/template/resources")) {
     if (json.pits && json.pits[0] && json.pits[0].delivery_materials) {
         json.pits[0].delivery_materials = [];
     }
-
     // 移除 pits[1]
     if (json.pits && json.pits[1]) {
         json.pits[1] = undefined;
     }
-
     // 移除 pits[1].delivery_materials
     if (json.pits && json.pits[1] && json.pits[1].delivery_materials) {
         json.pits[1].delivery_materials = [];
     }
-
     // 移除 pits[2].delivery_materials
     if (json.pits && json.pits[2] && json.pits[2].delivery_materials) {
         json.pits[2].delivery_materials = [];
@@ -27,9 +24,9 @@ if ($request.url.includes("/oc/exhibitions/template/resources")) {
 
 // 检查 URL 路径是否包含 /oc/exhibitions/app_mine/resources
 if ($request.url.includes("/oc/exhibitions/app_mine/resources")) {
-    // 移除 pits[2]
-    if (json.pits && json.pits[2]) {
-        json.pits[2] = undefined;
+    // 移除 pits[2].delivery_materials
+    if (json.pits && json.pits[2] && json.pits[2].delivery_materials) {
+        json.pits[2].delivery_materials = [];
     }
 }
 
