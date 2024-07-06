70b36d6b2945e6ea1de67a15e2b13bdcfb3573d5
palapal <palapal708@cumzle.com>
2024-07-06 22:52:04 +0800

    更新

--- Resource/Script/GaoDing/GaoDing_remove_ads.js
+++ Resource/Script/GaoDing/GaoDing_remove_ads.js
@@ -5,8 +5,8 @@ var json = JSON.parse($response.body);
 // 检查 URL 路径是否包含 /oc/exhibitions/template/resources
 if ($request.url.includes("/oc/exhibitions/template/resources")) {
     // 移除 pits[0].delivery_materials
-    if (json.pits && json.pits[0] && json.pits[0].delivery_materials) {
-        json.pits[0].delivery_materials = [];
+    if (json.pits && json.pits[0]) {
+        json.pits[0] = {};  // 首页轮播图
     }
     // 移除 pits[1]
     if (json.pits && json.pits[1]) {
@@ -15,11 +15,11 @@ if ($request.url.includes("/oc/exhibitions/template/resources")) {
 }
 
 // 检查 URL 路径是否包含 /oc/exhibitions/app_mine/resources
-// if ($request.url.includes("/oc/exhibitions/app_mine/resources")) {
-//     // 移除 pits[2]
-//     if (json.pits && json.pits[2]) {
-//         json.pits[2] = {};
-//     }
-// }
+if ($request.url.includes("/oc/exhibitions/app_mine/resources")) {
+    // 移除 pits[2]
+    if (json.pits && json.pits[2]) {
+        json.pits[2] = {};
+    }
+}
 
 $done({ body: JSON.stringify(json) });
