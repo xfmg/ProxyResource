306d78ce9bd1a1179de0cd211350cce232c4431b
palapal <palapal708@cumzle.com>
2024-07-06 22:58:39 +0800

    更新

--- Resource/Script/GaoDing/GaoDing_remove_ads.js
+++ Resource/Script/GaoDing/GaoDing_remove_ads.js
@@ -1,16 +1,27 @@
-// 2024-07-06 22:43:04
+// 2024-07-06 22:58:34
 
 var json = JSON.parse($response.body);
 
 // 检查 URL 路径是否包含 /oc/exhibitions/template/resources
 if ($request.url.includes("/oc/exhibitions/template/resources")) {
     // 移除 pits[0].delivery_materials
-    if (json.pits && json.pits[0]) {
-        json.pits[0] = {};  // 首页轮播图
+    if (json.pits && json.pits[0] && json.pits[0].delivery_materials) {
+        json.pits[0].delivery_materials = [];
     }
+
     // 移除 pits[1]
     if (json.pits && json.pits[1]) {
-        json.pits[1] = {};
+        json.pits[1] = undefined;
+    }
+
+    // 移除 pits[1].delivery_materials
+    if (json.pits && json.pits[1] && json.pits[1].delivery_materials) {
+        json.pits[1].delivery_materials = [];
+    }
+
+    // 移除 pits[2].delivery_materials
+    if (json.pits && json.pits[2] && json.pits[2].delivery_materials) {
+        json.pits[2].delivery_materials = [];
     }
 }
 
@@ -18,7 +29,7 @@ if ($request.url.includes("/oc/exhibitions/template/resources")) {
 if ($request.url.includes("/oc/exhibitions/app_mine/resources")) {
     // 移除 pits[2]
     if (json.pits && json.pits[2]) {
-        json.pits[2] = {};
+        json.pits[2] = undefined;
     }
 }
 
