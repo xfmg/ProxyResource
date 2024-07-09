2e89f1579cd6a8c50a2bc0846b1dd5cd31361f43
palapal <palapal708@cumzle.com>
2024-07-09 21:51:48 +0800

    更新

--- Resource/Script/VideoGo/VideoGo_remove_ads.js
+++ Resource/Script/VideoGo/VideoGo_remove_ads.js
@@ -1,15 +1,16 @@
-// 2024-07-09 17:30:04
+// 2024-07-09 21:51:21
+
 if ($request.url.includes("/valueadded/operation/config/master/station")) {
     var json = JSON.parse($response.body);
 
     // 删除 - 健康咨询
-    if (json.stationInfo && json.stationInfo.groupList && json.stationInfo.groupList[1] && json.stationInfo.groupList[1].serviceList && json.stationInfo.groupList[1].serviceList[9]) {
-        delete json.stationInfo.groupList[1].serviceList[9];
+    if (json.stationInfo && json.stationInfo.groupList && json.stationInfo.groupList[1] && json.stationInfo.groupList[1].serviceList && json.stationInfo.groupList[1].serviceList[9] && json.stationInfo.groupList[1].serviceList[9].showInfo) {
+        json.stationInfo.groupList[1].serviceList[9].showInfo = {};
     }
 
     // 删除 - 保险服务
-    if (json.stationInfo && json.stationInfo.groupList && json.stationInfo.groupList[1] && json.stationInfo.groupList[1].serviceList && json.stationInfo.groupList[1].serviceList[10]) {
-        delete json.stationInfo.groupList[1].serviceList[10];
+    if (json.stationInfo && json.stationInfo.groupList && json.stationInfo.groupList[1] && json.stationInfo.groupList[1].serviceList && json.stationInfo.groupList[1].serviceList[10] && json.stationInfo.groupList[1].serviceList[10].showInfo) {
+        json.stationInfo.groupList[1].serviceList[10].showInfo = {};
     }
 
     $done({ body: JSON.stringify(json) });
