699d428de727afa3eb26811f0fe58d25d14c5629
palapal <palapal708@cumzle.com>
2024-07-10 15:50:47 +0800

    更新

--- Resource/Script/QiXinBao/QiXinBao_remove_ads.js
+++ Resource/Script/QiXinBao/QiXinBao_remove_ads.js
@@ -1,9 +1,9 @@
-// 2024-07-10 15:42:44
+// 2024-07-10 15:50:37
 var json = JSON.parse($response.body);
 
-// 移除热门企业列表
+// 移除个性化推荐信息流
 if (json.data) {
-    delete json.data;
+    json.data = {};
 }
 
 $done({ body: JSON.stringify(json) });
