2d0ec1c09f973db5ff4b1b5825395b0ba71b4057
palapal <palapal708@cumzle.com>
2024-07-10 09:39:30 +0800

    更新

--- Resource/Script/FlyerTea/FlyerTea_remove_ads.js
+++ Resource/Script/FlyerTea/FlyerTea_remove_ads.js
@@ -2,6 +2,7 @@
 脚本引用 https://raw.githubusercontent.com/zirawell/Ad-Cleaner/main/Collection/js/flyert.js
 脚本时间 2024-07-10 08:18:08
 */
+/*
 let body = $response.body;
 let headers = $response.headers;
 const isResponse = typeof $response !== "undefined";
@@ -19,3 +20,29 @@ if(isResponse && isJson){
 }else{
   $done();
 }
+
+*/
+var json = JSON.parse($response.body);
+
+// 删除 Variables.data.xinren 路径
+delete json.Variables.data.xinren;
+
+// 删除 Variables.data 下指定路径的值
+var pathsToClear = [
+    "goodhotel",
+    "goodhotel_txt",
+    "goodthread",
+    "goodthread_txt",
+    "homeinns_already_joined",
+    "homeinns_entry",
+    "homeinns_entry_banner",
+    "homeinns_entry_url"
+];
+
+pathsToClear.forEach(function(path) {
+    if (json.Variables && json.Variables.data && json.Variables.data[path]) {
+        json.Variables.data[path] = {};
+    }
+});
+
+$done({ body: JSON.stringify(json) });
