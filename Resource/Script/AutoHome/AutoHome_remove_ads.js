23a8a644c6667f8c5e18b614106f413b867ca30c
palapal <palapal708@cumzle.com>
2024-07-17 11:31:36 +0800

    更新

--- Resource/Script/AutoHome/AutoHome_remove_ads.js
+++ Resource/Script/AutoHome/AutoHome_remove_ads.js
@@ -45,7 +45,7 @@ try {
     }
 
     // 删除二手车 - 竖版轮播图
-    if (/\/apic\/v\d+\/gethomepagefeed\//.test(url)) {
+    if (/\/apic\/v\d+\/gethomepagefeed/.test(url)) {
         console.log("删除车抵贷");
         obj = removeItemsWithKeywords(obj, "车抵贷");
     }
