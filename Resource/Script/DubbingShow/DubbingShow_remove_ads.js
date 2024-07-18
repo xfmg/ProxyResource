2065737ea123bd1691eb75e8d8484518c0eb5fa7
palapal <palapal708@cumzle.com>
2024-07-18 17:49:41 +0800

    更新

--- Resource/Script/DubbingShow/DubbingShow_remove_ads.js
+++ Resource/Script/DubbingShow/DubbingShow_remove_ads.js
@@ -1,12 +1,19 @@
-// 2024-07-18 17:27:32
+// 2024-07-18 17:49:22
+const url = $request.url;
 const body = $response.body;
 
 if (!body) $done({});
 
 let obj = JSON.parse(body);
 
-if (obj.data && obj.data.banners) {
-    delete obj.data.banners; // 移除语聊横幅
+if (url.includes("/Api/live/LiveList")) {
+    delete obj.data.banners; // 首页 - 推荐 - 语聊横幅
+} else if (url.includes("/api/live/FollowList")) {
+    delete obj.data; // 首页 - 推荐 - 语聊关注推荐
+} else if (url.includes("/Api/Friend/GetRecommendUsers")) {
+    delete obj.data; // 首页 - 关注推荐
+} else if (url.includes("/api/Union/HomeData")) {
+    delete obj.data; // 消息 - 群聊推荐
 }
 
 $done({ body: JSON.stringify(obj) });
