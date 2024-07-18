fb5f56b7952addabf8dbd9baca20033634448aa4
palapal <palapal708@cumzle.com>
2024-07-19 07:56:40 +0800

    更新

--- Resource/Script/BiliComic/BiliComic_remove_ads.js
+++ Resource/Script/BiliComic/BiliComic_remove_ads.js
@@ -1,4 +1,4 @@
-// 2024-07-19 07:37:16
+// 2024-07-19 07:55:52
 const url = $request.url;
 
 const processResponse = (regex, filterFunc) => {
@@ -12,13 +12,20 @@ const processResponse = (regex, filterFunc) => {
 };
 
 const filterNewcomerAndMall = obj => {
-    obj.data?.home_type?.filter(item => item.name !== "新人", "新作"); // 首页标签
+    obj.data?.home_type?.filter(item => item.name !== "新人" && item.name !== "新作"); // 首页标签
     obj.data?.home_feed?.filter(item => item.name !== "商城"); // 首页标签
 };
 
 const filterNoTraffic = obj => {
-    obj.data?.confs?.filter(item => item.title !== "漫画商城", "超漫俱乐部", "看漫免流量"); // 我的页面
+    obj.data?.confs?.filter(item => item.title !== "漫画商城" && item.title !== "超漫俱乐部" && item.title !== "看漫免流量"); // 我的页面
+};
+
+const filterNovelBanners = obj => {
+    if (obj.data && obj.data.banners) {
+        delete obj.data.banners; // 小说列表横幅
+    }
 };
 
 processResponse(/\/twirp\/comic\.v\d\.Comic\/GetClassPageAllTabs/, filterNewcomerAndMall);
 processResponse(/\/twirp\/user\.v\d\.User\/UCenterConf/, filterNoTraffic);
+processResponse(/\/twirp\/novel\.v\d\.Home\/Home/, filterNovelBanners);
