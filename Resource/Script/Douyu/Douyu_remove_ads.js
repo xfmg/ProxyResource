2975dacadbbf2c51a9bdce631f6ac908f5e54ab3
palapal <palapal708@cumzle.com>
2024-07-28 22:11:47 +0800

    更新

--- Resource/Script/Douyu/Douyu_remove_ads.js
+++ Resource/Script/Douyu/Douyu_remove_ads.js
@@ -2,37 +2,30 @@
 const url = $request.url;
 let obj = JSON.parse($response.body);
 
-
 if (url.includes("/mgapi/livenc/home/getRecV3")) {
-    // 首页轮播图和视频流广告
-    const clearAds = (items) => {
-        items.forEach(item => {
-            if (item.ad) {
-                item.ad = {};
-            }
-        });
-    };
+    function removeAds(data) {
+        if (data && Array.isArray(data)) {
+            return data.filter(item => !item.ad);
+        }
+        return data;
+    }
 
-    if (obj.data) {
-        if (obj.data.rec_cont) {
-            clearAds(obj.data.rec_cont);
-        }
-        if (obj.data.rec_card && obj.data.rec_card.card_banner) {
-            clearAds(obj.data.rec_card.card_banner);
-        }
+    if (obj.data && obj.data.rec_cont) {
+        obj.data.rec_cont = removeAds(obj.data.rec_cont);
+    }
+    if (obj.data && obj.data.rec_card && obj.data.rec_card.card_banner) {
+        obj.data.rec_card.card_banner = removeAds(obj.data.rec_card.card_banner);
     }
 }
 
 if (url.includes("/japi/entrance/roomRes/nc/m/list")) {
-    // 首页直播间悬浮窗
     if (obj.data) {
-        delete obj.data.pendant_a;
-        delete obj.data.entrance_d;
+        delete obj.data.pendant_a; // 直播间悬浮窗
+        delete obj.data.entrance_d; // 直播间宝箱
     }
 }
 
 if (/^\/venus\/config\/static\/update\?aid=ios&client_sys=ios&keyCodeSet=flow_config$/.test(url)) {
-    // 修改配置文件
     const keysToZero = [
         "greatGodGameSitterSwitch", // 大神游戏陪玩
         "followMoreAnchorEntrance", // 关注更多主播入口
@@ -41,13 +34,12 @@ if (/^\/venus\/config\/static\/update\?aid=ios&client_sys=ios&keyCodeSet=flow_co
         "bringGoodsSwitch", // 带货开关
         "qqGameSwitch" // QQ游戏
     ];
-    if (obj.data) {
-        keysToZero.forEach(key => {
-            if (obj.data.hasOwnProperty(key)) {
-                obj.data[key] = 0;
-            }
-        });
-    }
+
+    keysToZero.forEach(key => {
+        if (obj.data && obj.data.hasOwnProperty(key)) {
+            obj.data[key] = 0;
+        }
+    });
 }
 
 $done({ body: JSON.stringify(obj) });
