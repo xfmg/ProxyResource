ada8b5dcb85b4444cd0dcc2412909779acfea2ba
palapal <palapal708@cumzle.com>
2024-07-28 22:19:51 +0800

    更新

--- Resource/Script/Douyu/Douyu_remove_ads.js
+++ Resource/Script/Douyu/Douyu_remove_ads.js
@@ -1,13 +1,10 @@
-// 2024-07-28 22:03:26
+// 2024-07-28 22:19:42
 const url = $request.url;
 let obj = JSON.parse($response.body);
 
 if (url.includes("/mgapi/livenc/home/getRecV3")) {
     function removeAds(data) {
-        if (data && Array.isArray(data)) {
-            return data.filter(item => !item.ad);
-        }
-        return data;
+        return data.filter(item => !item.ad);
     }
 
     if (obj.data && obj.data.rec_cont) {
@@ -26,20 +23,22 @@ if (url.includes("/japi/entrance/roomRes/nc/m/list")) {
 }
 
 if (/^\/venus\/config\/static\/update\?aid=ios&client_sys=ios&keyCodeSet=flow_config$/.test(url)) {
-    const keysToZero = [
-        "greatGodGameSitterSwitch", // 大神游戏陪玩
-        "followMoreAnchorEntrance", // 关注更多主播入口
-        "sdklivebanner", // 直播横幅
-        "homeActFloatSwitch", // 首页活动悬浮窗
-        "bringGoodsSwitch", // 带货开关
-        "qqGameSwitch" // QQ游戏
-    ];
+    const keysToZero = {
+        "greatGodGameSitterSwitch": 0, // 大神游戏陪玩
+        "followMoreAnchorEntrance": 0, // 关注更多主播入口
+        "sdklivebanner": 0, // 直播横幅
+        "homeActFloatSwitch": 0, // 首页活动悬浮窗
+        "bringGoodsSwitch": 0, // 带货开关
+        "qqGameSwitch": 0 // QQ游戏
+    };
 
-    keysToZero.forEach(key => {
-        if (obj.data && obj.data.hasOwnProperty(key)) {
-            obj.data[key] = 0;
+    if (obj.data) {
+        for (let key in keysToZero) {
+            if (obj.data.hasOwnProperty(key)) {
+                obj.data[key] = keysToZero[key];
+            }
         }
-    });
+    }
 }
 
 $done({ body: JSON.stringify(obj) });
