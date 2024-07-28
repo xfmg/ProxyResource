3196d0022f2f96a42a186bcd20b1703cddfe966b
palapal <palapal708@cumzle.com>
2024-07-28 22:03:38 +0800

    更新

--- Resource/Script/Douyu/Douyu_remove_ads.js
+++ Resource/Script/Douyu/Douyu_remove_ads.js
@@ -1,44 +1,39 @@
-// 2024-07-28 21:56:40
+// 2024-07-28 22:03:26
 const url = $request.url;
 let obj = JSON.parse($response.body);
 
-function removeAds(data, paths) {
-    paths.forEach(path => {
-        let current = data;
-        const parts = path.split('.');
-        for (let i = 0; i < parts.length - 1; i++) {
-            if (!current[parts[i]]) break;
-            current = current[parts[i]];
-        }
-        if (current && current[parts[parts.length - 1]]) {
-            delete current[parts[parts.length - 1]];
-        }
-    });
-}
 
-function setKeysToZero(data, keys) {
-    keys.forEach(key => {
-        if (data[key] !== undefined) {
-            data[key] = 0;
-        }
-    });
-}
-
-// 首页轮播图和视频流广告
 if (url.includes("/mgapi/livenc/home/getRecV3")) {
-    const paths = ["data.rec_cont", "data.rec_card.card_banner"];
-    removeAds(obj, paths);
+    // 首页轮播图和视频流广告
+    const clearAds = (items) => {
+        items.forEach(item => {
+            if (item.ad) {
+                item.ad = {};
+            }
+        });
+    };
+
+    if (obj.data) {
+        if (obj.data.rec_cont) {
+            clearAds(obj.data.rec_cont);
+        }
+        if (obj.data.rec_card && obj.data.rec_card.card_banner) {
+            clearAds(obj.data.rec_card.card_banner);
+        }
+    }
 }
 
-// 首页直播间悬浮窗
 if (url.includes("/japi/entrance/roomRes/nc/m/list")) {
-    const paths = ["data.pendant_a", "data.entrance_d"];
-    removeAds(obj, paths);
+    // 首页直播间悬浮窗
+    if (obj.data) {
+        delete obj.data.pendant_a;
+        delete obj.data.entrance_d;
+    }
 }
 
-// 修改配置文件
-if (/\/venus\/config\/static\/update\?aid=ios&client_sys=ios&keyCodeSet=flow_config/.test(url)) {
-    const keys = [
+if (/^\/venus\/config\/static\/update\?aid=ios&client_sys=ios&keyCodeSet=flow_config$/.test(url)) {
+    // 修改配置文件
+    const keysToZero = [
         "greatGodGameSitterSwitch", // 大神游戏陪玩
         "followMoreAnchorEntrance", // 关注更多主播入口
         "sdklivebanner", // 直播横幅
@@ -46,7 +41,13 @@ if (/\/venus\/config\/static\/update\?aid=ios&client_sys=ios&keyCodeSet=flow_con
         "bringGoodsSwitch", // 带货开关
         "qqGameSwitch" // QQ游戏
     ];
-    setKeysToZero(obj.data, keys);
+    if (obj.data) {
+        keysToZero.forEach(key => {
+            if (obj.data.hasOwnProperty(key)) {
+                obj.data[key] = 0;
+            }
+        });
+    }
 }
 
 $done({ body: JSON.stringify(obj) });
