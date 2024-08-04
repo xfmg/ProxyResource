b5995a5662198257853b80fc4d778bddd063b70e
palapal <palapal708@cumzle.com>
2024-08-05 02:32:01 +0800

    更新

--- Resource/Script/GaoDing/GaoDing_remove_ads.js
+++ Resource/Script/GaoDing/GaoDing_remove_ads.js
@@ -1,4 +1,4 @@
-// 2024-08-05 02:25:14
+// 2024-08-05 02:31:55
 const url = $request.url;
 // 移除功能入口角标
 if (url.includes("/oc/exhibitions/templates/resources")) {
@@ -7,12 +7,8 @@ if (url.includes("/oc/exhibitions/templates/resources")) {
         obj.pits.forEach(pit => {
             if (pit.materials && Array.isArray(pit.materials)) {
                 pit.materials.forEach(material => {
-                    if (material.material && Array.isArray(material.material)) {
-                        material.material.forEach(item => {
-                            if (item.badge_img_url) {
-                                item.badge_img_url = "";
-                            }
-                        });
+                    if (material.material && material.material.badge_img_url) {
+                        material.material.badge_img_url = "";
                     }
                 });
             }
