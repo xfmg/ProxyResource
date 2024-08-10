dbbe465d31392c1df76963eda000f6ad1980c954
palapal <palapal708@cumzle.com>
2024-08-10 22:56:48 +0800

    更新

--- Resource/Script/FleaMarket/FleaMarket_remove_ads.js
+++ Resource/Script/FleaMarket/FleaMarket_remove_ads.js
@@ -1,25 +1,17 @@
 // 2024-08-10 22:33:37
 const url = $request.url;
+if (!$response.body) $done({});
 let obj = JSON.parse($response.body);
 
 if (url.includes("/gw/mtop.taobao.idlehome.home.nextfresh")) {
-    function traverseAndRemove(data, path, targetValue) {
-        const keys = path.split('.');
-        let current = data;
-        for (let i = 0; i < keys.length; i++) {
-            if (i === keys.length - 1) {
-                if (current[keys[i]] === targetValue) {
-                    return null;
-                }
-            } else {
-                current = current[keys[i]];
-                if (!current) break;
+    if (obj.data && obj.data.sections) {
+        obj.data.sections = obj.data.sections.filter(section => {
+            if (section.template && section.template.name === "fish_home_advertise_card_d4") {
+                return false; // 删除该数组项
             }
-        }
-        return data;
+            return true; // 保留该数组项
+        });
     }
-
-    obj = traverseAndRemove(obj, 'data.sections.template.name', 'fish_home_advertise_card_d4');
 }
 
 $done({ body: JSON.stringify(obj) });
