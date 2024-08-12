8759806b4a0cca3b632fb795497c09bb77c32db7
palapal <palapal708@cumzle.com>
2024-08-12 22:24:22 +0800

    更新

--- Resource/Script/CatEarFM/CatEarFM_remove_ads.js
+++ Resource/Script/CatEarFM/CatEarFM_remove_ads.js
@@ -1,11 +1,27 @@
-// 2024-08-12 20:33:39
+// 2024-08-12 22:21:49
 const url = $request.url;
-if (url.includes("/site/icons")) {
-    let obj = JSON.parse($response.body);
-    if (obj.info && obj.info.icons && Array.isArray(obj.info.icons)) {
-        obj.info.icons = obj.info.icons.filter(icon => icon.title !== "直播" && icon.title !== "周边商城");
-    }
-    $done({ body: JSON.stringify(obj) });
-} else {
-    $done({});
+if (!$response.body) $done({});
+let obj = JSON.parse($response.body);
+
+// 首页标签
+function filterIcons(icons) {
+    return icons.filter(icon => icon.title !== "直播" && icon.title !== "周边商城");
 }
+
+function filterInfo(info, titlesToRemove) {
+    return info.filter(item => !titlesToRemove.includes(item.title));
+}
+
+// 发现页面
+if (url.includes("/site/icons")) {
+    if (obj.info && obj.info.icons && Array.isArray(obj.info.icons)) {
+        obj.info.icons = filterIcons(obj.info.icons);
+    }
+} else if (url.includes("/discovery/list")) {
+    const titlesToRemove = ["直播间", "广播剧", "免流服务"];
+    if (obj.info && Array.isArray(obj.info)) {
+        obj.info = filterInfo(obj.info, titlesToRemove);
+    }
+}
+
+$done({ body: JSON.stringify(obj) });
