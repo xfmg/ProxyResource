07de8ef5b7e025da31b7943a177036229569643c
palapal <palapal708@cumzle.com>
2024-08-17 01:54:53 +0800

    更新

--- Resource/Script/MaiMai/MaiMai_remove_ads.js
+++ Resource/Script/MaiMai/MaiMai_remove_ads.js
@@ -1,21 +1,22 @@
-// 2024-08-17 01:48:25
+// 2024-08-17 01:54:45
 const url = $request.url;
 let obj = JSON.parse($response.body);
 
 if (url.includes("/maimai/feed/v6/feed_detail_comment?")) {
-    // 遍历 lst 路径，如果存在 newAdStyle 键名，则删除当前 newAdStyle 所在的数组
-    if (obj.lst) {
-        obj.lst = obj.lst.filter(item => !item.hasOwnProperty("newAdStyle"));
+    if (obj.lst && Array.isArray(obj.lst)) {
+        obj.lst = obj.lst.filter(item => !item.hasOwnProperty("newAdStyle")); // 信息流广告
+    }
+} else if (url.includes("/maimai/gossip/v3/gossip_detail_comment?")) {
+    if (obj.comments && obj.comments.lst && Array.isArray(obj.comments.lst)) {
+        obj.comments.lst = obj.comments.lst.filter(item => !item.hasOwnProperty("newAdStyle")); // 信息流广告
     }
 } else if (url.includes("/maimai/feed/v6/feed_detail_header?")) {
-    // 遍历 feed.style1.link_card 路径，如果存在 link_card 键名，则删除当前 link_card 所在的数组
-    if (obj.feed && obj.feed.style1 && obj.feed.style1.link_card) {
-        obj.feed.style1.link_card = obj.feed.style1.link_card.filter(item => !item.hasOwnProperty("link_card"));
+    if (obj.feed && obj.feed.style1 && Array.isArray(obj.feed.style1.link_card)) {
+        obj.feed.style1.link_card = []; // 卡片广告
     }
 } else if (url.includes("/maimai/feed/v5/focus_feed?")) {
-    // 遍历 feeds.style1.link_card 路径，如果存在 link_card 键名，则删除当前 link_card 所在的数组
-    if (obj.feeds && obj.feeds.style1 && obj.feeds.style1.link_card) {
-        obj.feeds.style1.link_card = obj.feeds.style1.link_card.filter(item => !item.hasOwnProperty("link_card"));
+    if (obj.feeds && obj.feeds.style1 && Array.isArray(obj.feeds.style1.link_card)) {
+        obj.feeds.style1.link_card = []; // 卡片广告
     }
 }
 
