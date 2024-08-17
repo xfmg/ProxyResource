18f1397cccefdc0a0e972e0e7e4fe18dec8a40da
palapal <palapal708@cumzle.com>
2024-08-18 00:35:14 +0800

    更新

--- Resource/Script/MaiMai/MaiMai_remove_ads.js
+++ Resource/Script/MaiMai/MaiMai_remove_ads.js
@@ -1,14 +1,18 @@
-// 2024-08-17 23:35:34
+// 2024-08-18 00:34:59
 const url = $request.url;
 let obj = JSON.parse($response.body);
 
 if (url.includes("/maimai/feed/v5/focus_feed?")) {
-    if (obj.feeds && Array.isArray(obj.feeds)) {
-        obj.feeds = obj.feeds.filter(feed => !feed.newAdStyle); // 信息流大块广告
+    if (obj.feeds && Array.isArray(obj.feeds.newAdStyle)) {
+        obj.feeds.newAdStyle = obj.feeds.newAdStyle.filter(item => !item.hasOwnProperty('newAdStyle'));
     }
 } else if (url.includes("/maimai/gossip/v3/gossip_detail_comment?")) {
     if (obj.comments && obj.comments.lst && Array.isArray(obj.comments.lst)) {
-        obj.comments.lst = obj.comments.lst.filter(comment => !comment.newAdStyle); // 评论区大块广告
+        obj.comments.lst = obj.comments.lst.filter(item => !item.hasOwnProperty('newAdStyle'));
+    }
+} else if (url.includes("/maimai/feed/v6/feed_detail_comment?")) {
+    if (obj.lst && Array.isArray(obj.lst)) {
+        obj.lst = obj.lst.filter(item => !item.hasOwnProperty('newAdStyle'));
     }
 }
 
