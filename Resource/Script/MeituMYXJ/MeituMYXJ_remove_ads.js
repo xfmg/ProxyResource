d8ffa92a0c3e96d3280ba885c1f18517edc2dbae
palapal <palapal708@cumzle.com>
2024-09-07 23:03:00 +0800

    更新

--- Resource/Script/MeituMYXJ/MeituMYXJ_remove_ads.js
+++ Resource/Script/MeituMYXJ/MeituMYXJ_remove_ads.js
@@ -1,14 +1,16 @@
-// 2024-09-07 22:50:43
+// 2024-09-07 23:02:42
 const url = $request.url;
 if (!$response.body) $done({});
 let obj = JSON.parse($response.body);
 
 if (url.includes("/operation/home.json")) {
     // 遍历 func_list
-    if (obj.response.list && obj.response.list.func_list) {
-        obj.response.list.func_list = obj.response.list.func_list.filter(item => {
-            return item.name !== "借钱" && item.name !== "AI剪辑";
+    if (obj.response.list) {
+      for (let i in obj.response.list) {
+        obj.response.list[i].func_list = obj.response.list[i].func_list.filter(item => {
+          return item.name !== "借钱" && item.name !== "AI剪辑";
         });
+      }
     }
 
     // 遍历 func_bar_type
@@ -18,5 +20,3 @@ if (url.includes("/operation/home.json")) {
         });
     }
 }
-
-$done({ body: JSON.stringify(obj) });
