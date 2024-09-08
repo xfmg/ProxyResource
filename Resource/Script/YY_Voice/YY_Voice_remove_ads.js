fb660931aa38cb22bfa0a63bedac8c49de271d6f
palapal <palapal708@cumzle.com>
2024-09-08 11:43:18 +0800

    更新

--- Resource/Script/YY_Voice/YY_Voice_remove_ads.js
+++ Resource/Script/YY_Voice/YY_Voice_remove_ads.js
@@ -1,33 +1,25 @@
-// 2024-09-08 11:01:15
+// 2024-09-08 11:42:51
 const url = $request.url;
-if (!$response.body) $done({});
 let obj = JSON.parse($response.body);
 
 if (url.includes("/personalCenter/list")) {
-    // 处理Banner轮播模块
+    // 删除 moduleName 为 "Banner轮播" 的对象
     if (obj.data && obj.data.modules) {
-        obj.data.modules = obj.data.modules.filter(module => {
-            if (module.moduleName === "Banner轮播") {
-                console.log("删除Banner轮播模块");
-                return false;
-            }
-            return true;
-        });
+        obj.data.modules = obj.data.modules.filter(module => module.moduleName !== "Banner轮播");
     }
 
-    // 处理度小满借钱和免流量入口
+    // 删除 entrances 中 name 为 "度小满借钱" 或 "免流量" 的对象
     if (obj.data && obj.data.modules) {
         obj.data.modules.forEach(module => {
             if (module.entrances) {
-                module.entrances = module.entrances.filter(entrance => {
-                    if (["度小满借钱", "免流量"].includes(entrance.name)) {
-                        console.log(`删除${entrance.name}入口`);
-                        return false;
-                    }
-                    return true;
-                });
+                module.entrances = module.entrances.filter(entrance => !["度小满借钱", "免流量"].includes(entrance.name));
             }
         });
+    }
+} else if (url.includes("/tabs")) {
+    // 删除 name 为 "直播" 或 "短剧" 的对象
+    if (obj.data && obj.data.tabs) {
+        obj.data.tabs = obj.data.tabs.filter(tab => !["直播", "短剧"].includes(tab.name));
     }
 }
 
