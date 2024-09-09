d8394074031e47f9d4efd0c042ff7b5dc4fa1400
palapal <palapal708@cumzle.com>
2024-09-09 09:43:31 +0800

    更新

--- Resource/Script/PiPiXia/PiPiXia_remove_ads.js
+++ Resource/Script/PiPiXia/PiPiXia_remove_ads.js
@@ -1,35 +1,20 @@
-// 2024-09-09 09:30:00
+// 2024-09-09 09:43:18
 const url = $request.url;
 let obj = JSON.parse($response.body);
 
+// 处理 /bds/feed/stream/v2/ 路径
 if (url.includes("/bds/feed/stream/v2/")) {
-    // 遍历 data.data.cell_type 路径
     if (obj.data && obj.data.data && Array.isArray(obj.data.data)) {
-        obj.data.data = obj.data.data.filter(item => {
-            if (item.cell_type === 2) {
-                console.log("删除 cell_type 为 2 的对象");
-                return false;
-            }
-            return true;
-        });
+        obj.data.data = obj.data.data.filter(item => !item.ad_info);
     }
 }
 
+// 处理 /bds/user/check_in/ 路径
 if (url.includes("/bds/user/check_in/")) {
-    // 遍历 data.profile_entrances.title 路径
     if (obj.data && obj.data.profile_entrances && Array.isArray(obj.data.profile_entrances)) {
-        obj.data.profile_entrances = obj.data.profile_entrances.filter(item => {
-            if (item.title === "放心借" || item.title === "洋钱罐借款") {
-                console.log(`删除 title 为 ${item.title} 的对象`);
-                return false;
-            }
-            return true;
-        });
+        obj.data.profile_entrances = obj.data.profile_entrances.filter(item => item.title !== "放心借" && item.title !== "洋钱罐借款");
     }
-
-    // 删除 data.pet_interface_message 路径
     if (obj.data && obj.data.pet_interface_message) {
-        console.log("删除 pet_interface_message 路径");
         delete obj.data.pet_interface_message;
     }
 }
