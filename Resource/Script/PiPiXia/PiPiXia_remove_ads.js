327b3fcb020565ff057663f6186b27d998fe1e28
palapal <palapal708@cumzle.com>
2024-09-09 09:30:12 +0800

    更新

--- Resource/Script/PiPiXia/PiPiXia_remove_ads.js
+++ Resource/Script/PiPiXia/PiPiXia_remove_ads.js
@@ -1,42 +1,37 @@
-/*
-脚本引用https://raw.githubusercontent.com/ZenmoFeiShi/Qx/main/PPX.js
-*/
-// 2024-02-02 13:51
+// 2024-09-09 09:30:00
 const url = $request.url;
-const scriptEnvironment = typeof $task != 'undefined' ? 'Surge' : (typeof $loon != 'undefined' ? 'Loon' : (typeof $httpClient != 'undefined' ? 'Qx' : 'Unknown'));
-
-if (!$response.body || scriptEnvironment === 'Unknown') {
-  $done({});
-}
-
 let obj = JSON.parse($response.body);
 
-
-function filterProfileEntrances() {
-  let profileEntrances = obj.data.profile_entrances;
-  let titlesToFilter = ['放心借', '创作中心', '原创特权', '小黑屋', '我的订单'];
-
-  obj.data.profile_entrances = profileEntrances.filter(entry => !titlesToFilter.includes(entry.title));
-  fixPos(obj.data.profile_entrances);
+if (url.includes("/bds/feed/stream/v2/")) {
+    // 遍历 data.data.cell_type 路径
+    if (obj.data && obj.data.data && Array.isArray(obj.data.data)) {
+        obj.data.data = obj.data.data.filter(item => {
+            if (item.cell_type === 2) {
+                console.log("删除 cell_type 为 2 的对象");
+                return false;
+            }
+            return true;
+        });
+    }
 }
 
-function filterChannelModel() {
-  if (obj.data.channel_model) {
-    obj.data.channel_model = obj.data.channel_model.filter(item => ["feed", "image_text"].includes(item.event_name));
-    fixPos(obj.data.channel_model);
-  }
-}
+if (url.includes("/bds/user/check_in/")) {
+    // 遍历 data.profile_entrances.title 路径
+    if (obj.data && obj.data.profile_entrances && Array.isArray(obj.data.profile_entrances)) {
+        obj.data.profile_entrances = obj.data.profile_entrances.filter(item => {
+            if (item.title === "放心借" || item.title === "洋钱罐借款") {
+                console.log(`删除 title 为 ${item.title} 的对象`);
+                return false;
+            }
+            return true;
+        });
+    }
 
-function fixPos(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    arr[i].pos = i + 1;
-  }
-}
-
-  if (url.includes("/bds/user/check_in/")) {
-  filterProfileEntrances();
-} else if (url.includes("/bds/feed/channel_list/")) {
-  filterChannelModel();
+    // 删除 data.pet_interface_message 路径
+    if (obj.data && obj.data.pet_interface_message) {
+        console.log("删除 pet_interface_message 路径");
+        delete obj.data.pet_interface_message;
+    }
 }
 
 $done({ body: JSON.stringify(obj) });
